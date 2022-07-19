abcd9a8e5cd99398e4fdf32e45e1e197847f6437
hnhtran <tran_eetas@yahoo.com>
2022-07-20 01:59:23 +1000

    main_app views update

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+# from .models import Magnet
 
 class Magnet:
     def __init__(self, name, type, description, year):
@@ -18,3 +19,14 @@ magnets = [
 
 def home(request):
     return HttpResponse('<h1>You are at the home page. Welcome World! Welcome Magnet World!</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
+def magnets_index(request):
+    # magnets = Magnet.objects.all()
+    return render(request, 'cats/index.html', { 'magnets': magnets })
+
+# def magnets_detail(request, magnet_id):
+#     magnet = Magnet.objects.get(id=cat_id)
+#     return render(request, 'cats/detail.html', { 'magnet': magnet })
