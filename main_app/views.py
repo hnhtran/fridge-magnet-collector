2f9c7c655daa20df73dff5b46c420b60faba194f
hnhtran <tran_eetas@yahoo.com>
2022-07-19 23:04:04 +1000

    homeviews update

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+class Magnet:
+    def __init__(self, name, type, description, year):
+        self.name = name
+        self.type = type
+        self.descripton = description
+        self.year = year
+
+magnets = [
+    Magnet('Castle', 'travel', 'Denmark', '2017')
+    Magnet('Pizza Hut', 'food', 'pizza', '2019')
+    Magnet('ACountants', 'business card', 'ACountants', '2021')
+]
+
+def home(request):
+    return HttpResponse('<h1>You are at the home page. Welcome World! Welcome Magnet World!</h1>')
