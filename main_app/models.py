a60e75b092b5334751181dbd90b6806431ffc20a
hnhtran <tran_eetas@yahoo.com>
2022-07-20 05:52:15 +1000

    models update

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Magnet(models.Model):
+    name = models.CharField(max_length=50)
+    kind = models.CharField(max_length=50)
+    description = models.TextField(max_length=250)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'magnet_id': self.id})
