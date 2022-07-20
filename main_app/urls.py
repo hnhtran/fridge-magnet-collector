71c1cd3de57388b8d6c37b5609e704d331c09c18
hnhtran <tran_eetas@yahoo.com>
2022-07-21 07:04:55 +1000

    update urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('magnets/<int:magnet_id>', views.magnets_detail, name='detail'),
     path('magnets/create', views.MagnetCreate.as_view(), name='magnets_create'),
     path('magnets/<int:pk>/update', views.MagnetUpdate.as_view(), name='magnets_update'),
-    path('magnets/<int:pk>/delete', views.MagnetDelete.as_view(), name='magnets_delete')
+    path('magnets/<int:pk>/delete', views.MagnetDelete.as_view(), name='magnets_delete'),
+    path('magnets/<int:magnet_id>/add_surface', views.add_surface, name='add_surface'),
 ]
